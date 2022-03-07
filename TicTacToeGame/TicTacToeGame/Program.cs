f1690a29822993fb7652910b7d3b9d9737e2b607
ronit#123 <ronitwaje23>
2022-03-07 10:09:53 +0530

    [Ronit]Initialize Board

---
+++ TicTacToeGame/TicTacToeGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the tic tac toe Game");
+
+
+            TicTacToeGame.startplay();
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ TicTacToeGame/TicTacToeGame/TicTacToeGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    internal class TicTacToeGame
+    {
+        public static char[] TicTacToeBoard;
+
+        public static void startplay()
+        {
+
+            Console.WriteLine("game starting");
+            Console.WriteLine("==================");
+            Console.WriteLine("||  |    |   ||");
+            Console.WriteLine("||  |    |   ||");
+            Console.WriteLine("||  |    |   ||");
+            Console.WriteLine("==================");
+
+            TicTacToeBoard = new char[10] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+            TicTacToeBoard = TicTacToeBoard.Skip(1).ToArray();
+            Console.ReadLine();
+
+        }
+    }
+}
